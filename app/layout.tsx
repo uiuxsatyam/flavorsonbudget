71ef5a1da72651145704f990893162b5af2ba4fd
Satyam Anand <satyam.anand007@gmail.com>
2026-02-25 17:18:35 +0530

    style: set official logo as site icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,19 +17,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
+        url: '/images/logo.png',
+        sizes: 'any',
+      }
     ],
-    apple: '/apple-icon.png',
+    apple: '/images/logo.png',
   },
   verification: {
     other: {
